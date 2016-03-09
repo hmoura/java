97ecff2e96fb125fccb123adf7b754aff7eed182
hivisonmoura <hivisonmoura@192.168.1.148>
2016-03-08 23:31:10 -0300

    Operador Ternario  - Curso em Videos

---
+++ Documents/Java/OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorternario;
+
+/**
+ *
+ * @author hivisonmoura
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1, n2;
+        n1 = 4;
+        n2 = 8;
+        int r;
+        r = (n1 > n2)?n1:n2;
+        System.out.println(r);
+    }
+    
+}
